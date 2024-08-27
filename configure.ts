be275e14174be9665443c065883c164016568e8d
Aditya Darma <adhit.boys1@gmail.com>
2024-08-27 21:23:23 +0800

    update

--- configure.ts
+++ configure.ts
@@ -13,5 +13,47 @@
 */
 
 import ConfigureCommand from '@adonisjs/core/commands/configure'
+import { stubsRoot } from './stubs/main.js'
 
-export async function configure(_command: ConfigureCommand) {}
+export async function configure(command: ConfigureCommand) {
+  const codemods = await command.createCodemods()
+
+  /**
+   * Publish config file
+   */
+  await codemods.makeUsingStub(stubsRoot, 'config/storage.stub', {})
+
+  /**
+   * Add environment variables
+   */
+  await codemods.defineEnvVariables({
+    /*
+    |----------------------------------------------------------
+    | Variables for configuring AWS S3
+    |----------------------------------------------------------
+    */
+    AWS_S3_KEY: '',
+    AWS_S3_SECRET: '',
+    AWS_S3_REGION: '',
+    AWS_S3_BUCKET: '',
+  })
+
+  /**
+   * Validate environment variables
+   */
+  await codemods.defineEnvValidations({
+    variables: {
+      AWS_S3_KEY: `Env.schema.string.optional()`,
+      AWS_S3_SECRET: 'Env.schema.string.optional()',
+      AWS_S3_REGION: 'Env.schema.string.optional()',
+      AWS_S3_BUCKET: 'Env.schema.string.optional()',
+    },
+  })
+
+  /**
+   * Add provider to rc file
+   */
+  await codemods.updateRcFile((rcFile) => {
+    rcFile.addProvider('@adityadarma/adonis-storage/storage_provider')
+  })
+}
